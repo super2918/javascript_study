ba4593bb323f90e2ffe3fa85475f6d717d4fcd59
mc2918 <tlsdud1375@gmail.com>
2021-01-08 14:43:14 +0900

    Add corrdinamtes in broswer project and update  html, css, markdown, js

---
+++ project/broswer/coordinates/main.js
@@ -0,0 +1,16 @@
+// (function(){
+//   const $startButton = document.querySelector('.start');
+//   const $stopButton = document.querySelector('.stop');
+//   const 
+
+//   function startCoordinate() {
+    
+//   }
+
+//   function init() {
+    
+//   }
+
+//   init();
+
+// })();
